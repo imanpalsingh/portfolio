c5830ae52dffee0fcb3ff89a6d9281fb9d50e0c8
Imanpal Singh <imanpal@kiprosh.com>
2022-10-22 21:59:04 +0530

    add gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,30 @@
-import type { GatsbyConfig } from "gatsby";
+import type {GatsbyConfig} from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `ImanpalSingh`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `Tiro Bangla`,
+            file: `https://fonts.googleapis.com/css2?family=Tiro+Bangla&display=swap`,
+          },
+        ],
+      },
+    },
+  ],
 };
 
 export default config;
